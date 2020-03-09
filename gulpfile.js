54ab5257ce11cd46a2d0d651de740f2992fdcab6
David Romero <geekzolanos@gmail.com>
2020-03-09 02:36:09 -0400

    Actualizacion de gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 /* Needed gulp config */
 
+var fs = require('fs');
 var gulp = require('gulp');  
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
@@ -8,18 +9,16 @@ var notify = require('gulp-notify');
 var minifycss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
+var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
 
-/* Setup scss path */
-var paths = {
-    scss: './sass/*.scss'
-};
-
-/* Scripts task */
-gulp.task('scripts', function() {
+/* vendor-js task */
+gulp.task('vendor-js', function() {
   return gulp.src([
     /* Add your JS files here, they will be combined in this order */
     'node_modules/jquery/dist/jquery.min.js',
@@ -30,101 +29,79 @@ gulp.task('scripts', function() {
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/waypoints/lib/jquery.waypoints.min.js'
     ])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('js'))
+    .pipe(concat('vendor.js'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    //.pipe(uglify())
+    .pipe(gulp.dest('build/scripts'));
 });
 
-gulp.task('minify-main', function() {
-  return gulp.src([
-    /* Add your JS files here, they will be combined in this order */
-    'js/main.js'
-    ])
+gulp.task('js', function() {
+  return gulp.src(['src/js/main.js', 'src/js/custom.js'])
     .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    //.pipe(uglify())
+    .pipe(gulp.dest('build/scripts'));
 });
 
 /* Sass task */
 gulp.task('sass', function () {  
-    return gulp.src('scss/style.scss')
-    .pipe(plumber())
-    .pipe(sass({
-      errLogToConsole: true,
+    return gulp.src(['src/scss/vendor.scss', 'src/scss/main.scss'])
+        .pipe(plumber())
+        .pipe(sass({
+            errLogToConsole: true,
 
-      //outputStyle: 'compressed',
-      // outputStyle: 'compact',
-      // outputStyle: 'nested',
-      outputStyle: 'expanded',
-      precision: 10
-    }))
-
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('css'))
-
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('css'))
-    /* Reload the browser CSS after every change */
-    .pipe(reload({stream:true}));
-});
-
-gulp.task('merge-styles', function () {
-
-    return gulp.src([
-        'css/vendor/bootstrap.min.css',
-        'css/vendor/animate.css',
-        'css/vendor/icomoon.css',
-        'css/vendor/flexslider.css',
-        'css/vendor/owl.carousel.min.css',
-        'css/vendor/owl.theme.default.min.css',
-        'css/vendor/magnific-popup.css',
-        'css/vendor/photoswipe.css',
-        'css/vendor/default-skin.css',
-        'fonts/icomoon/style.css',
-        ])
-        // .pipe(sourcemaps.init())
-        // .pipe(autoprefixer({
-        //     browsers: ['last 2 versions'],
-        //     cascade: false
-        // }))
-        .pipe(concat('styles-merged.css'))
-        .pipe(gulp.dest('css'))
-        // .pipe(rename({suffix: '.min'}))
-        // .pipe(minifycss())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('css'))
+            //outputStyle: 'compressed',
+            // outputStyle: 'compact',
+            // outputStyle: 'nested',
+            outputStyle: 'expanded',
+            precision: 10
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(rename({suffix: '.min'}))
+        //.pipe(minifycss())
+        .pipe(gulp.dest('build/style'))
+        /* Reload the browser CSS after every change */
         .pipe(reload({stream:true}));
 });
 
+gulp.task('html', function() {
+    return gulp.src('src/views/*.njk')
+        .pipe(data(() => JSON.parse(fs.readFileSync('src/data.json'))))
+        .pipe(nunjucksRender({path: ['src/views']}))
+        .pipe(gulp.dest('build'));
+});
+
 /* Reload task */
-gulp.task('bs-reload', function () {
+gulp.task('bs-reload', function (done) {
     browserSync.reload();
+    done();
 });
 
 /* Prepare Browser-sync for localhost */
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: ["build", 'src']
         }
     });
 });
 
 gulp.task('watch', function() {
     /* Watch scss, run the sass task on change. */
-    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass']);
-    /* Watch app.js file, run the scripts task on change. */
-    gulp.watch(['js/main.js'], ['minify-main']);
+    gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], gulp.series('sass', 'bs-reload'));
+    /* Watch app.js file, run the vendor-js task on change. */
+    gulp.watch(['src/js/main.js'], gulp.series('js', 'bs-reload'));
     /* Watch .html files, run the bs-reload task on change. */
-    gulp.watch(['*.html'], ['bs-reload']);
+    gulp.watch(['src/views/*.njk', 'src/views/**/*.njk'], gulp.series('html', 'bs-reload'));
 });
 
+function clean() {
+    return del('build');
+}
+
 /* Watch scss, js and html files, doing different things with each. */
-gulp.task('default', gulp.series('sass', 'scripts', 'browser-sync', 'watch'));
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel('html', 'sass', 'vendor-js', 'js'), 
+    gulp.parallel('browser-sync', 'watch')
+));
